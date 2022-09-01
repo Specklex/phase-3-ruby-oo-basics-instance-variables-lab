a99c32fa5b8b263d4acd61035a409d818ff357c0
Jaden Lund <lundjaden@gmail.com>
2022-09-01 12:43:06 -0400

    commit tree 594f2990c289d95a383487e114e9e21c5ba42d87

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,11 @@
 class Dog
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+    #  The first method takes in an argument of a dog's name and assigns that value to a variable, this_dogs_name.
 
+    def name  
+        @this_dogs_name
+    end
+    # The second method is responsible for reporting the name.
 end
